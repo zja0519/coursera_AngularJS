4fb595c3f1ff20572a34216c13a1ce25e0d31b74
Jiao Zhang <jiaozhang@Jiaos-MacBook-Pro.local>
2016-10-16 17:03:00 -0700

    resolve with http works

--- mod4_solution/src/js/menudata.service.js
+++ mod4_solution/src/js/menudata.service.js
@@ -9,30 +9,37 @@ angular.module('Data')
 MenuDataService.$inject = ['$http', 'ApiBasePath']
 function MenuDataService($http, ApiBasePath) {
   var service = this;
-  var categoryItems=[];
 
   service.getAllCategories = function() {
-    if(categoryItems.length === 0) {
-      $http({
+    // if(categoryItems.length === 0) {
+    //   $http({
+    //       method: "GET",
+    //       url: (ApiBasePath + "/categories.json")
+    //   }).then(function (response) {
+    //     // process result and only keep items that match
+    //     var items = response.data;
+    //     for(var i in items) {
+    //       categoryItems.push(items[i].name);
+    //     }
+    //   }).catch(function (error) {
+    //   });
+    //   return
+    // }
+    // return categoryItems;
+    // return ["1","2","3"];
+      return $http({
           method: "GET",
           url: (ApiBasePath + "/categories.json")
       }).then(function (response) {
         // process result and only keep items that match
         var items = response.data;
+        var categoryItems=[];
         for(var i in items) {
           categoryItems.push(items[i].name);
         }
+        return categoryItems;
       }).catch(function (error) {
       });
-    }
-    return categoryItems;
-    // return ["1","2","3"];
-      // var response = $http({
-      //     method: "GET",
-      //     url: (ApiBasePath + "/categories.json")
-      // });
-      //
-      // return response.name;
   };
 
   service.getItemsForCategory = function(categoryShortName) {
